5f6a2760d68f93c6e16ee4b70ac5d8c8046ff59b
vladg94 <vladgeorgescu13@gmail.com>
2021-09-09 19:02:37 +0300

    progress

---
+++ ssg/parsers.py
@@ -0,0 +1,31 @@
+from typing import List
+from pathlib import Path
+import shutil
+
+class Parser:
+    extensions: List[str] = []
+
+    def valid_extension(self, extension):
+        return True if extension in self.extensions else False
+
+    def parse(self, path, source: Path, dest: Path):
+        raise NotImplementedError
+    
+    def read(self, path):
+        with open(path, 'r') as file:
+            file.read_text()
+    
+    def write(self, path, dest, content, ext=".html"):
+        full_path = dest / path.with_suffix(ext)
+        with open(full_path, 'w') as file:
+            file.write(content)
+
+    def copy(self, path, source, dest):
+        shutil.copy2(path, dest / path.relative_to(source))
+    
+class ResourceParser(Parser):
+    extensions = ['.jpg', '.png', '.gif', '.css', '.html']
+
+    def parse(self, path, source: Path, dest: Path):
+        Parser.copy(path, source, dest)
+
--- ssg/site.py
+++ ssg/site.py
@@ -2,9 +2,10 @@ from os import path
 from pathlib import Path
 
 class Site:
-    def __init__(self, source, dest):
+    def __init__(self, source, dest, parsers=None):
         self.source = Path(source)
         self.dest = Path(dest)
+        self.parsers = parsers or []
 
     def create_dir(self, path):
         directory = self.dest / path.relative_to(self.source)
@@ -18,7 +19,23 @@ class Site:
         for path in self.source.rglob("*"):
             if path.is_dir():
                 self.create_dir(path)
+            elif path.is_file():
+                self.run_parser
+    
+    def load_parser(self, extension):
+        for parser in self.parsers:
+            if parser.valid_extension(extension):
+                return parser
             else:
                 pass
+    
+    def run_parser(self, path):
+        parser = self.load_parser(path.suffix)
+        if parser is not None:
+            parser.parse(path, self.source, self.dest)
+        else:
+            print('Not Implemented')
+
+    
 
 
