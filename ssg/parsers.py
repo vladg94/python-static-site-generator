2603807f4d397d1d8fdf5f7ff61e402c5de6a94c
vladg94 <vladgeorgescu13@gmail.com>
2021-09-10 14:49:35 +0300

    last mod parsers

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -8,15 +8,15 @@ class Parser:
     def valid_extension(self, extension):
         return True if extension in self.extensions else False
 
-    def parse(self, path, source: Path, dest: Path):
+    def parse(self, path: Path, source: Path, dest: Path):
         raise NotImplementedError
     
     def read(self, path):
         with open(path, 'r') as file:
-            file.read_text()
+            return file.read()
     
     def write(self, path, dest, content, ext=".html"):
-        full_path = dest / path.with_suffix(ext)
+        full_path = dest / path.with_suffix(ext).name 
         with open(full_path, 'w') as file:
             file.write(content)
 
--- ssg/site.py
+++ ssg/site.py
@@ -20,7 +20,7 @@ class Site:
             if path.is_dir():
                 self.create_dir(path)
             elif path.is_file():
-                self.run_parser
+                self.run_parser(path)
     
     def load_parser(self, extension):
         for parser in self.parsers:
